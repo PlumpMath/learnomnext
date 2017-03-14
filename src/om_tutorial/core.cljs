87f5182c1fa114fac10a775a682bf8ec42038cea
Thinh P. Tran <trphthinh@gmail.com>
2017-03-14 20:03:02 +0700

    Add reconciler

--- src/om_tutorial/core.cljs
+++ src/om_tutorial/core.cljs
@@ -7,7 +7,7 @@
 
 (println "Hello World!")
 
-(defonce app-state (atom {:count 0}))
+(def app-state (atom {:count 0}))
 
 (defui HelloWorld
   Object
@@ -22,6 +22,26 @@
                        :title (str "Hello" %)})
               (range 3)))
   (gdom/getElement "app"))
+
+;; A Naive Design
+(defui Counter
+  Object
+  (render [this]
+    (let [{:keys [count]} (om/props this)]
+      (dom/div nil
+         (dom/span nil (str "Count: " count))
+         (dom/button
+           #js {:onClick
+                (fn [e]
+                  (swap! app-state update-in [:count] inc))}
+           "Click me!")))))
+
+(def reconciler
+  (om/reconciler
+    {:state app-state}))
+
+(om/add-root! reconciler
+   Counter (gdom/getElement "app1"))
 
 
 
